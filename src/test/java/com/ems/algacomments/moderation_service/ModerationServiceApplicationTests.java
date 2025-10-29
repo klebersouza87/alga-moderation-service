b48023fa15d62cfe1dfae361de926cc97bf2d58e
Kleber Souza <kleber.souza@any.mail.com.br>
2025-10-29 08:25:01 -0300

    Added initial java project files.

---
+++ src/test/java/com/ems/algacomments/moderation_service/ModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.algacomments.moderation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
